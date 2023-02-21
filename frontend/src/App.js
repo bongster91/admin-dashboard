2a9c444dfd0a67c8932afd37e78a63b996380d46
Grace Bong <gracebong.park@gmail.com>
2023-02-21 16:52:58 -0500

    more boilerplate added with routes and global theme

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,34 @@
+import { useMemo } from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import { createTheme } from '@mui/material/styles';
+import { themeSettings } from 'theme';
+
+import Dashboard from 'scenes/Dashboard';
 
 function App() {
+    const mode = useSelector((state) => state.global.mode);
+    const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+
     return (
         <div className="app">
-            Admin Analytics Frontend
+            <BrowserRouter>
+                <ThemeProvider theme={theme}>
+                    <CssBaseline />
+
+                    <Routes>
+                        <Route element={ <Layout /> }>
+                            <Route path='/' element={ <Navigate to='/dashboard' replace /> } />
+                            <Route path='/dashboard' element={ <Dashboard /> } />
+                        </Route>
+                    </Routes>
+
+                </ThemeProvider>
+            </BrowserRouter>
         </div>
     );
-}
+};
 
 export default App;
