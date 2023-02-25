14494d3bcf558ebbcea1e70a8e4acfa4967787d3
Grace Bong <gracebong.park@gmail.com>
2023-02-25 17:07:37 -0500

    created queries to call product info from mongodb

--- backend/routes/client.js
+++ backend/routes/client.js
@@ -1,5 +1,8 @@
 import express from 'express';
+import { getProducts } from '../controllers/client.js';
 
 const clientRouter = express.Router();
+
+router.get('/products', getProducts);
 
 export default clientRouter;
