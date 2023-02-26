6cd2ec75bf79942bb76718421873c5e4a6275cfb
Grace Bong <gracebong.park@gmail.com>
2023-02-26 14:42:39 -0500

    created all dashboard stat boxes

--- frontend/src/scenes/dashboard/index.jsx
+++ frontend/src/scenes/dashboard/index.jsx
@@ -1,8 +1,136 @@
 import React from 'react';
+import { useGetDashboardQuery } from 'state/api';
+
+import {
+    DownloadOutlined,
+    Email,
+    PointOfSale,
+    PersonAdd,
+    Traffic,
+} from '@mui/icons-material';
+import {
+    Box,
+    Button,
+    Typography,
+    useTheme,
+    useMediaQuery,
+} from '@mui/material';
+import { DataGrid } from '@mui/x-data-grid';
+
+import FlexBetween from 'components/FlexBetween';
+import Header from 'components/Header';
+import BreakdownChart from 'components/BreakdownChart';
+import OverviewChart from 'components/OverviewChart';
+import StatBox from 'components/StatBox';
 
 const Dashboard = () => {
+    const theme = useTheme();
+    const isNonMediumScreens = useMediaQuery('(min-width: 1200px)');
+    const { data, isLoading } = useGetDashboardQuery();
+
+    const columns = [
+        {
+            field: '_id',
+            headerName: 'ID',
+            flex: 1
+        },
+        {
+            field: 'userId',
+            headerName: 'USER ID',
+            flex: 1
+        },
+        {
+            field: 'createdAt',
+            headerName: 'CreatedAt',
+            flex: 1
+        },
+        {
+            field: 'products',
+            headerName: '# of Products',
+            flex: 0.5,
+            sortable: false,
+            renderCell: (params) => params.value.length
+        },
+        {
+            field: 'cost',
+            headerName: 'Cost',
+            flex: 1,
+            renderCell: (params) => `$${Number(params.value).toFixed(2)}`
+        },
+    ];
+
     return (
-        <div>DASHBOARD PAGE</div>
+        <Box m='1.5rem 2.5rem'>
+            <FlexBetween>
+                <Header title='DASHBOARD' subtitle='Welcome to Your Dashboard' />
+                
+                <Box>
+                    <Button
+                        sx={{
+                            backgroundColor: theme.palette.secondary.light,
+                            color: theme.palette.background.alt,
+                            fontSize: '14px',
+                            fontWeight: 'bold',
+                            padding: '10px 20px',
+                        }}
+                    >
+                        <DownloadOutlined sx={{ mr: '10px' }} />
+                        Dowload Reports
+                    </Button>
+                </Box>
+            </FlexBetween>
+
+            <Box
+                mt='20px'
+                display='grid'
+                gridTemplateColumns='repeat(12, 1fr)'
+                gridAutoRows='160px'
+                gap='20px'
+                sx={{
+                    '& > div': {
+                        gridColumn: isNonMediumScreens ? undefined : 'span 12'
+                    }
+                }}
+            >
+                {/* ROW 1 */}
+                <StatBox
+                    title='Total Customers'
+                    value={data && data.totalCustomers}
+                    increase='+14%'
+                    description='Since last month'
+                    icon={
+                        <Email sx={{ color: theme.palette.secondary[300], fontSize: '26px' }} />
+                    }
+                />
+                <StatBox
+                    title='Sales Today'
+                    value={data && data.todayStats.totalSales}
+                    increase='+21%'
+                    description='Since last month'
+                    icon={
+                        <PointOfSale sx={{ color: theme.palette.secondary[300], fontSize: '26px' }} />
+                    }
+                />
+                <StatBox
+                    title='Monthly Sales'
+                    value={data && data.thisMonthSales.totalSales}
+                    increase='+5%'
+                    description='Since last month'
+                    icon={
+                        <PersonAdd sx={{ color: theme.palette.secondary[300], fontSize: '26px' }} />
+                    }
+                />
+                <StatBox
+                    title='Yearly Sales'
+                    value={data && data.yearlySalesTotal}
+                    increase='+43%'
+                    description='Since last month'
+                    icon={
+                        <Traffic sx={{ color: theme.palette.secondary[300], fontSize: '26px' }} />
+                    }
+                />
+            </Box>
+        </Box>
     );
 };
 
