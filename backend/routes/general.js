e01f30263e8e1921ba7d06c2782a6563096f5c81
Grace Bong <gracebong.park@gmail.com>
2023-02-26 14:40:59 -0500

    dashboard mongo route/controller created

--- backend/routes/general.js
+++ backend/routes/general.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import { getUser } from '../controllers/general.js';
+import { getUser, getDashboardStats } from '../controllers/general.js';
 
 const generalRouter = express.Router();
 
 generalRouter.get('/user/:id', getUser);
+generalRouter.get('/dashboard', getDashboardStats);
 
 export default generalRouter;
