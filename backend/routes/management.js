ac7515656da6eed6e3a5bba3133ae528891c5e2f
Grace Bong <gracebong.park@gmail.com>
2023-02-26 13:59:17 -0500

    created admin and performance aggregation routes to mongodb

--- backend/routes/management.js
+++ backend/routes/management.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import { getAdmins } from '../controllers/management.js';
+import { getAdmins, getUserPerformance } from '../controllers/management.js';
 
 const managementRouter = express.Router();
 
 managementRouter.get('/admins', getAdmins);
+managementRouter.get('/performance/:id', getUserPerformance);
 
 export default managementRouter;
