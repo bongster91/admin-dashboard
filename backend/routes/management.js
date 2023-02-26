b74a5e9e105ce2734517f4fb8b4e2ecc5053edf1
Grace Bong <gracebong.park@gmail.com>
2023-02-26 13:07:37 -0500

    created management admins route and controller

--- backend/routes/management.js
+++ backend/routes/management.js
@@ -1,5 +1,8 @@
 import express from 'express';
+import { getAdmins } from '../controllers/management.js';
 
 const managementRouter = express.Router();
+
+managementRouter.get('/admins', getAdmins);
 
 export default managementRouter;
